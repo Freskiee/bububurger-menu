f776318e056cbd1a34daf8b2d342fc6804cf7595
Freskiee <freska_96@hotmail.com>
2025-06-12 23:51:13 -0600

    factura

--- src/components/InvoiceModal.tsx
+++ src/components/InvoiceModal.tsx
@@ -123,25 +123,25 @@ const InvoiceModal: React.FC<InvoiceModalProps> = ({ show, onHide }) => {
                         required
                       >
                         <option value="">Selecciona tu Régimen Fiscal</option>
-                        <option value="601">601 - General de Ley Personas Morales</option>
-                        <option value="603">603 - Personas Morales con Fines no Lucrativos</option>
-                        <option value="605">605 - Sueldos y Salarios e Ingresos Asimilados a Salarios</option>
-                        <option value="606">606 - Arrendamiento</option>
-                        <option value="607">607 - Régimen de Enajenación o Adquisición de Bienes</option>
-                        <option value="608">608 - Demás Ingresos</option>
-                        <option value="610">610 - Residentes en el Extranjero sin Establecimiento Permanente en México</option>
-                        <option value="611">611 - Ingresos por Dividendos (Socios y Accionistas)</option>
-                        <option value="612">612 - Personas Físicas con Actividades Empresariales y Profesionales</option>
-                        <option value="614">614 - Ingresos por Intereses</option>
-                        <option value="615">615 - Régimen de los Ingresos por Obtención de Premios</option>
-                        <option value="616">616 - Sin Obligaciones Fiscales</option>
-                        <option value="620">620 - Sociedades Cooperativas de Producción que optan por diferir sus ingresos</option>
-                        <option value="621">621 - Incorporación Fiscal</option>
-                        <option value="622">622 - Actividades Agrícolas, Ganaderas, Silvícolas y Pesqueras</option>
-                        <option value="623">623 - Opcional para Grupos de Sociedades</option>
-                        <option value="624">624 - Coordinados</option>
-                        <option value="625">625 - Régimen de las Actividades Empresariales con Ingresos a través de Plataformas Tecnológicas</option>
-                        <option value="626">626 - Régimen Simplificado de Confianza</option>
+                        <option value="General de Ley Personas Morales">601 - General de Ley Personas Morales</option>
+                        <option value="Personas Morales con Fines no Lucrativos">603 - Personas Morales con Fines no Lucrativos</option>
+                        <option value="Sueldos y Salarios e Ingresos Asimilados a Salarios">605 - Sueldos y Salarios e Ingresos Asimilados a Salarios</option>
+                        <option value="Arrendamiento">606 - Arrendamiento</option>
+                        <option value="Régimen de Enajenación o Adquisición de Bienes">607 - Régimen de Enajenación o Adquisición de Bienes</option>
+                        <option value="Demás Ingresos">608 - Demás Ingresos</option>
+                        <option value="Residentes en el Extranjero sin Establecimiento Permanente en México">610 - Residentes en el Extranjero sin Establecimiento Permanente en México</option>
+                        <option value="Ingresos por Dividendos (Socios y Accionistas)">611 - Ingresos por Dividendos (Socios y Accionistas)</option>
+                        <option value="Personas Físicas con Actividades Empresariales y Profesionales">612 - Personas Físicas con Actividades Empresariales y Profesionales</option>
+                        <option value="Ingresos por Intereses">614 - Ingresos por Intereses</option>
+                        <option value="Régimen de los Ingresos por Obtención de Premios">615 - Régimen de los Ingresos por Obtención de Premios</option>
+                        <option value="Sin Obligaciones Fiscales">616 - Sin Obligaciones Fiscales</option>
+                        <option value="Sociedades Cooperativas de Producción que optan por diferir sus ingresos">620 - Sociedades Cooperativas de Producción que optan por diferir sus ingresos</option>
+                        <option value="Incorporación Fiscal">621 - Incorporación Fiscal</option>
+                        <option value="Actividades Agrícolas, Ganaderas, Silvícolas y Pesqueras">622 - Actividades Agrícolas, Ganaderas, Silvícolas y Pesqueras</option>
+                        <option value="Opcional para Grupos de Sociedades">623 - Opcional para Grupos de Sociedades</option>
+                        <option value="Coordinados">624 - Coordinados</option>
+                        <option value="Régimen de las Actividades Empresariales con Ingresos a través de Plataformas Tecnológicas">625 - Régimen de las Actividades Empresariales con Ingresos a través de Plataformas Tecnológicas</option>
+                        <option value="Régimen Simplificado de Confianza">626 - Régimen Simplificado de Confianza</option>
                       </select>
                     </div>
 
@@ -155,11 +155,11 @@ const InvoiceModal: React.FC<InvoiceModalProps> = ({ show, onHide }) => {
                         required
                       >
                         <option value="">Selecciona Método de Pago</option>
-                        <option value="01">01 - Efectivo</option>
-                        <option value="03">03 - Transferencia Electrónica de Fondos</option>
-                        <option value="04">04 - Tarjeta de Crédito</option>
-                        <option value="28">28 - Tarjeta de Débito</option>
-                        <option value="99">99 - Por Definir</option>
+                        <option value="Efectivo">01 - Efectivo</option>
+                        <option value="Transferencia Electrónica de Fondos">03 - Transferencia Electrónica de Fondos</option>
+                        <option value="Tarjeta de Crédito">04 - Tarjeta de Crédito</option>
+                        <option value="Tarjeta de Débito">28 - Tarjeta de Débito</option>
+                        <option value="Por Definir">99 - Por Definir</option>
                       </select>
                     </div>
 
