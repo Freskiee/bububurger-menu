5db11982547e13f8ca04aac4d9f8eb8be0f6ba75
Freskiee <freska_96@hotmail.com>
2025-06-20 20:40:40 -0600

    imagenesCocteles

--- src/components/CocktailGrid.tsx
+++ src/components/CocktailGrid.tsx
@@ -17,84 +17,84 @@ const COCKTAILS: Product[] = [
     name: 'El Coscorrón',
     description: 'Bacardí de Coco, Leche Deslactosada, Calahua y Coco Rallado.',
     prices: [{ label: 'Precio', value: 160 }],
-    image: '/src/assets/menu-images/coscorron.png'
+    image: '/menu-images/coscorron.png'
   },
   {
     id: 'a4_2',
     name: 'El Diablo',
     description: 'Tequila, Agua Mineral, Limón, Jarabe Natural, Vino Tinto y Escarchado de Chamoy con Tajín.',
     prices: [{ label: 'Precio', value: 145 }],
-    image: '/src/assets/menu-images/diablo-coc.png'
+    image: '/menu-images/diablo-coc.png'
   },
   {
     id: 'a4_3',
     name: 'La Conga',
     description: 'Ron Malibu, Jugo de Piña, Jugo de Naranja y Granadina.',
     prices: [{ label: 'Precio', value: 145 }],
-    image: '/src/assets/menu-images/conga.png'
+    image: '/menu-images/conga.png'
   },
   {
     id: 'a4_4',
     name: 'La Chaqueta',
     description: 'Mezcal Espadín Joven, Calahua, Jugo de Piña y Granadina, Shakeado.',
     prices: [{ label: 'Precio', value: 160 }],
-    image: '/src/assets/menu-images/chaqueta.png'
+    image: '/menu-images/chaqueta.png'
   },
   {
     id: 'a4_5',
     name: 'El Sugar Daddy',
     description: 'Ron Bacardí Blanco, Whisky, Tequila, Ginebra, Limón, Jugo de Arándano y Escarchado de Fresa.',
     prices: [{ label: 'Precio', value: 165 }],
-    image: '/src/assets/menu-images/sugar-daddy.png'
+    image: '/menu-images/sugar-daddy.png'
   },
   {
     id: 'a4_6',
     name: 'Paloma de Fresa',
     description: 'Tequila Reposado, Jarabe de Fresa, Fresas, Refresco de Toronja, Chamoy, Tajín, Limón y Sal.',
     prices: [{ label: 'Precio', value: 165 }],
-    image: '/src/assets/menu-images/paloma-fresa.png'
+    image: '/menu-images/paloma-fresa.png'
   },
   {
     id: 'a4_7',
     name: 'Paloma de Mango',
     description: 'Tequila Reposado, Jarabe de Mango, Mango, Refresco de Toronja, Chamoy, Tajín, Limón y Sal.',
     prices: [{ label: 'Precio', value: 165 }],
-    image: '/src/assets/menu-images/paloma-mango.png'
+    image: '/menu-images/paloma-mango.png'
   },
   {
     id: 'a4_8',
     name: 'Gin Frutos Rojos',
     description: 'Ginebra, Jarabe de Frutos Rojos, Frutos Rojos y Agua Tónica.',
     prices: [{ label: 'Precio', value: 170 }],
-    image: '/src/assets/menu-images/gin-frutosr.png'
+    image: '/menu-images/gin-frutosr.png'
   },
   {
     id: 'a4_9',
     name: 'Gin Mango',
     description: 'Ginebra, Jarabe de Mango, Mango y Agua Tónica.',
     prices: [{ label: 'Precio', value: 170 }],
-    image: '/src/assets/menu-images/gin-mango.png'
+    image: '/menu-images/gin-mango.png'
   },
   {
     id: 'a4_10',
     name: 'La Pepa Rosada',
     description: 'Ron Bacardí Blanco, Leche Evaporada, Leche Deslactosada, Jarabe de Cerezas, Granadina, Canela en Polvo y Cerezas.',
     prices: [{ label: 'Precio', value: 165 }],
-    image: '/src/assets/menu-images/pepa-rosada.png'
+    image: '/menu-images/pepa-rosada.png'
   },
   {
     id: 'a4_11',
     name: 'Margarita Frozen',
     description: 'Tequila, Triple Seco; de Fresa, Mango o Tamarindo, Escarchado de Tajín.',
     prices: [{ label: 'Precio', value: 180 }],
-    image: '/src/assets/menu-images/margarita.png'
+    image: '/menu-images/margarita.png'
   },
   {
     id: 'a4_12',
     name: 'Whisky Summer',
     description: 'Whisky, Jarabe Natural, Limón, Agua Mineral, Shakeado.',
     prices: [{ label: 'Precio', value: 180 }],
-    image: '/src/assets/menu-images/whisky-summer.png'
+    image: '/menu-images/whisky-summer.png'
   }
 ];
 
